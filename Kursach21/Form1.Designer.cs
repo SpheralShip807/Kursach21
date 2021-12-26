fe6080dc72a0c14d5fbc38225823bcfcc1dc56b9
Dzirt DoUrden <79597378+SpheralShip807@users.noreply.github.com>
2021-12-26 22:10:05 +0300

    Сумма, произведение и дизъюнкция

--- Kursach21/Form1.Designer.cs
+++ Kursach21/Form1.Designer.cs
@@ -29,13 +29,208 @@ namespace Kursach21
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.CreateTables = new System.Windows.Forms.Button();
+            this.CountValues = new System.Windows.Forms.Button();
+            this.DrawGrafs = new System.Windows.Forms.Button();
+            this.FirstTable = new System.Windows.Forms.DataGridView();
+            this.SecondTable = new System.Windows.Forms.DataGridView();
+            this.AlgProiz = new System.Windows.Forms.DataGridView();
+            this.AlgSum = new System.Windows.Forms.DataGridView();
+            this.Dizun = new System.Windows.Forms.DataGridView();
+            this.TablesHeight = new System.Windows.Forms.TextBox();
+            this.TablesWeight = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.FirstTable)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SecondTable)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AlgProiz)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AlgSum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dizun)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CreateTables
+            // 
+            this.CreateTables.Location = new System.Drawing.Point(5, 394);
+            this.CreateTables.Name = "CreateTables";
+            this.CreateTables.Size = new System.Drawing.Size(75, 44);
+            this.CreateTables.TabIndex = 0;
+            this.CreateTables.Text = "Create Tables";
+            this.CreateTables.UseVisualStyleBackColor = true;
+            this.CreateTables.Click += new System.EventHandler(this.CreateTables_Click);
+            // 
+            // CountValues
+            // 
+            this.CountValues.Location = new System.Drawing.Point(86, 394);
+            this.CountValues.Name = "CountValues";
+            this.CountValues.Size = new System.Drawing.Size(75, 44);
+            this.CountValues.TabIndex = 1;
+            this.CountValues.Text = "Count Values";
+            this.CountValues.UseVisualStyleBackColor = true;
+            this.CountValues.Click += new System.EventHandler(this.CountValues_Click);
+            // 
+            // DrawGrafs
+            // 
+            this.DrawGrafs.Location = new System.Drawing.Point(167, 394);
+            this.DrawGrafs.Name = "DrawGrafs";
+            this.DrawGrafs.Size = new System.Drawing.Size(75, 44);
+            this.DrawGrafs.TabIndex = 2;
+            this.DrawGrafs.Text = "Draw Grafs";
+            this.DrawGrafs.UseVisualStyleBackColor = true;
+            // 
+            // FirstTable
+            // 
+            this.FirstTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.FirstTable.Location = new System.Drawing.Point(4, 12);
+            this.FirstTable.Name = "FirstTable";
+            this.FirstTable.RowTemplate.Height = 25;
+            this.FirstTable.Size = new System.Drawing.Size(240, 150);
+            this.FirstTable.TabIndex = 3;
+            // 
+            // SecondTable
+            // 
+            this.SecondTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.SecondTable.Location = new System.Drawing.Point(4, 168);
+            this.SecondTable.Name = "SecondTable";
+            this.SecondTable.Size = new System.Drawing.Size(240, 150);
+            this.SecondTable.TabIndex = 4;
+            // 
+            // AlgProiz
+            // 
+            this.AlgProiz.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.AlgProiz.Location = new System.Drawing.Point(284, 12);
+            this.AlgProiz.Name = "AlgProiz";
+            this.AlgProiz.Size = new System.Drawing.Size(240, 150);
+            this.AlgProiz.TabIndex = 5;
+            // 
+            // AlgSum
+            // 
+            this.AlgSum.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.AlgSum.Location = new System.Drawing.Point(530, 12);
+            this.AlgSum.Name = "AlgSum";
+            this.AlgSum.Size = new System.Drawing.Size(240, 150);
+            this.AlgSum.TabIndex = 6;
+            // 
+            // Dizun
+            // 
+            this.Dizun.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Dizun.Location = new System.Drawing.Point(776, 12);
+            this.Dizun.Name = "Dizun";
+            this.Dizun.Size = new System.Drawing.Size(240, 150);
+            this.Dizun.TabIndex = 7;
+            // 
+            // TablesHeight
+            // 
+            this.TablesHeight.Location = new System.Drawing.Point(4, 324);
+            this.TablesHeight.Name = "TablesHeight";
+            this.TablesHeight.Size = new System.Drawing.Size(100, 23);
+            this.TablesHeight.TabIndex = 8;
+            // 
+            // TablesWeight
+            // 
+            this.TablesWeight.Location = new System.Drawing.Point(144, 324);
+            this.TablesWeight.Name = "TablesWeight";
+            this.TablesWeight.Size = new System.Drawing.Size(100, 23);
+            this.TablesWeight.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 350);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 15);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Высота";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(144, 350);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Ширина";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(284, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(179, 15);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Алгеброическое произведение";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(530, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(137, 15);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Алгеброическая сумма";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(776, 168);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 15);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1034, 451);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TablesWeight);
+            this.Controls.Add(this.TablesHeight);
+            this.Controls.Add(this.Dizun);
+            this.Controls.Add(this.AlgSum);
+            this.Controls.Add(this.AlgProiz);
+            this.Controls.Add(this.SecondTable);
+            this.Controls.Add(this.FirstTable);
+            this.Controls.Add(this.DrawGrafs);
+            this.Controls.Add(this.CountValues);
+            this.Controls.Add(this.CreateTables);
+            this.MaximumSize = new System.Drawing.Size(1050, 490);
+            this.MinimumSize = new System.Drawing.Size(1050, 490);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.FirstTable)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SecondTable)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AlgProiz)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AlgSum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dizun)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button CreateTables;
+        private System.Windows.Forms.Button CountValues;
+        private System.Windows.Forms.Button DrawGrafs;
+        private System.Windows.Forms.DataGridView FirstTable;
+        private System.Windows.Forms.DataGridView SecondTable;
+        private System.Windows.Forms.DataGridView AlgProiz;
+        private System.Windows.Forms.DataGridView AlgSum;
+        private System.Windows.Forms.DataGridView Dizun;
+        private System.Windows.Forms.TextBox TablesHeight;
+        private System.Windows.Forms.TextBox TablesWeight;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- Kursach21/Form1.cs
+++ Kursach21/Form1.cs
@@ -15,7 +15,111 @@ namespace Kursach21
         public Form1()
         {
             InitializeComponent();
+
+            //Запрет создавать новые строки
+            FirstTable.AllowUserToAddRows = false;
+            SecondTable.AllowUserToAddRows = false;
+
+            //Делаем единый размер ячеек
+            FirstTable.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            FirstTable.AutoResizeColumns();
+            FirstTable.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+
+            SecondTable.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            SecondTable.AutoResizeColumns();
+            SecondTable.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+
+            AlgProiz.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            AlgProiz.AutoResizeColumns();
+            AlgProiz.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+
+            AlgSum.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            AlgSum.AutoResizeColumns();
+            AlgSum.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+
+            Dizun.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            Dizun.AutoResizeColumns();
+            Dizun.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
         }
 
+        //Задаем кол-во строк и столбцов
+        private void CreateTables_Click(object sender, EventArgs e)
+        {
+            var heightOfTables = Convert.ToInt32(TablesHeight.Text);
+            var weightOfTables = Convert.ToInt32(TablesWeight.Text);
+
+            FirstTable.ColumnCount = weightOfTables;
+            FirstTable.RowCount = heightOfTables;
+            
+            SecondTable.ColumnCount = weightOfTables;
+            SecondTable.RowCount = heightOfTables;
+
+            AlgProiz.ColumnCount = weightOfTables;
+            AlgProiz.RowCount = heightOfTables;
+
+            AlgSum.ColumnCount = weightOfTables;
+            AlgSum.RowCount = heightOfTables;
+
+            Dizun.ColumnCount = weightOfTables;
+            Dizun.RowCount = heightOfTables;
+
+            //Рандом для тестов
+            var random = new Random();
+
+            for(int i = 0; i < heightOfTables; i++)
+            {
+                for(int j = 0; j < weightOfTables; j++)
+                {
+                    FirstTable.Rows[i].Cells[j].Value = Math.Round(random.NextDouble(), 1);
+                    SecondTable.Rows[i].Cells[j].Value = Math.Round(random.NextDouble(), 1);
+                }
+            }
+        }
+
+        private void CountValues_Click(object sender, EventArgs e)
+        {
+            //Рабочая переменная
+            double value;
+
+            //Алгеброическое произведение
+            for (int i = 0; i < FirstTable.RowCount; i++)
+            {
+                for (int j = 0; j < FirstTable.ColumnCount; j++)
+                {
+                    value = Convert.ToDouble(FirstTable.Rows[i].Cells[j].Value) * Convert.ToDouble(SecondTable.Rows[i].Cells[j].Value);
+
+                    AlgProiz.Rows[i].Cells[j].Value = Math.Round(value, 1);
+                }
+            }
+
+            //Алгеброическая сумма
+            for (int i = 0; i < FirstTable.RowCount; i++)
+            {
+                for (int j = 0; j < FirstTable.ColumnCount; j++)
+                {
+                    value = Convert.ToDouble(FirstTable.Rows[i].Cells[j].Value) + Convert.ToDouble(SecondTable.Rows[i].Cells[j].Value)
+                        - Convert.ToDouble(FirstTable.Rows[i].Cells[j].Value) * Convert.ToDouble(SecondTable.Rows[i].Cells[j].Value);
+                    if (value >= 1)
+                        AlgSum.Rows[i].Cells[j].Value = 1;
+                    else
+                        AlgSum.Rows[i].Cells[j].Value = Math.Round(value, 1);
+                }
+            }
+
+            //Дизъюктивная сумма
+            for (int i = 0; i < FirstTable.RowCount; i++)
+            {
+                for (int j = 0; j < FirstTable.ColumnCount; j++)
+                {
+                    //Для простоты работы
+                    var first = Convert.ToDouble(FirstTable.Rows[i].Cells[j].Value);
+                    var second = Convert.ToDouble(SecondTable.Rows[i].Cells[j].Value);
+
+                    value = Math.Max(Math.Min(first, 1 - second), Math.Min(1 - first, second));
+
+                    Dizun.Rows[i].Cells[j].Value = Math.Round(value, 1);
+                }
+            }
+        }
     }
 }
